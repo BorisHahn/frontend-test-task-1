d264359605d243a9114107c3345ad01288ad697f
Boris Gaidukevich <Borishahn@yandex.ru>
2022-10-29 03:36:21 +0300

    0.0.2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,45 +1,59 @@
 import React, { useState, useEffect } from "react";
+import Pagination from "@mui/material/Pagination";
 import ProductItem from "./ProductItem";
 import "./App.css";
 import "../src/font.css";
 
 function App() {
-  let url = `http://testtask.alto.codes/front-products.php`;
-
   const [productList, setProductList] = useState([]);
 
-  function getJSON() {
-    fetch(url)
+  function getJSON(page = 1) {
+    return fetch(
+      `http://testtask.alto.codes/front-products.php?skip=${(page - 1) * 4}`
+    )
       .then((response) => response.json())
       .then((data) => {
-        console.log(data.products);
-      return data.products;
-    });
+        return data.products;
+      });
   }
 
-  // const onProductListLoaded = (newProductList) => {
-  //   setProductList((productList) =>[...productList, ...newProductList]);
-  // };
+  const getCards = (page) => {
+    getJSON(page)
+      .then((newProductList) => {
+        setProductList([...newProductList]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   useEffect(() => {
-    getJSON()
+    getCards();
   }, []);
 
-  // const renderItems = (arr) => {
-  //   const items = arr.map((item) => {
-  //     return (
-  //       <li key={item.id}>
-  //         <img />
-  //         <div>{item.name}</div>
-  //       </li>
-  //     );
-  //   });
-  //   return <ul>{items}</ul>;
-  // };
+  const renderItems = (arr) => {
+    const items = arr.map((item) => {
+      return <ProductItem key={item.id} item={item} />;
+    });
+    return (
+      <div className="wrapper">
+        <ul className="product-list">{items}</ul>
+        <Pagination
+          className="pugination"
+          count={10}
+          color="primary"
+          defaultPage={1}
+          onChange={function (event, page) {
+            getCards(page);
+          }}
+        />
+      </div>
+    );
+  };
 
-  // const items = renderItems(productList);
+  const items = renderItems(productList);
 
-  return <div className="App">{<ProductItem />}</div>;
+  return <div className="App">{items}</div>;
 }
 
 export default App;
--- my-app/src/ProductItem.js
+++ my-app/src/ProductItem.js
@@ -1,18 +1,24 @@
 import "./ProductItem.css";
-import logo from "./img/logo.png";
+import inStock from "./img/inStock.png";
+import onOrder from "./img/onOrder.png";
 
-const ProductItem = () => {
+const ProductItem = (product) => {
+  const { availability, color, id, image_url, name, price, short_desc } =
+    product.item;
+const statusIcon = availability ? inStock : onOrder;
   return (
-    <div className="item">
-      <img src="http://testtask.alto.codes/img/test-product.jpeg" className="item__image" alt="Изображение товара" />
-      <img src={logo} className="item__status" alt="Статус товара" />
-      <p className="item__title">Наименование товара 1</p>
-      <p className="item__price">1000 ₽</p>
-      <p className="item__color">Цвет - Тифани</p>
-      <span className="item__description">Краткое описание товара</span>
+    <div className="item" key={id}>
+      <div className="item__image-wrapper">
+      <img src={image_url} className="item__image" alt="Изображение товара" />
+      </div>
+      <img src={statusIcon} className="item__status" alt="Статус товара" />
+      <p className="item__title">{name}</p>
+      <p className="item__price">{price}</p>
+      <p className="item__color">Цвет - {color}</p>
+      <span className="item__description">{short_desc}</span>
       <button className="item__button">В КОРЗИНУ</button>
     </div>
-  )
-}
+  );
+};
 
 export default ProductItem;
